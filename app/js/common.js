b8f433947b8ab6490b88d8d7a02671c93afd1ea6
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-02-04 19:53:49 +0300

    message

--- app/js/common.js
+++ app/js/common.js
@@ -1,34 +1,46 @@
+"use strict";
+
 // MOBILE MENU
 
 const mobileMenuCheck = document.querySelector('#checkbox');
 const mobileMenuCont = document.querySelector('#mobile-cont');
+const mobileLink = document.getElementsByClassName('mobile-menu-link');
 
 mobileMenuCheck.addEventListener('click', function () {
-  let activeClass = mobileMenuCont.classList.contains('mobile-menu-container--active');
-  if (!activeClass) {
-    mobileMenuCont.classList.add('mobile-menu-container--active');
-    document.body.style.overflow = 'hidden';
-  }
-  if (activeClass) {
-    mobileMenuCont.classList.remove('mobile-menu-container--active');
-    document.body.style.overflow = 'auto';
-  }
+    let activeClass = mobileMenuCont.classList.contains('mobile-menu-container--active');
+    if (!activeClass) {
+        mobileMenuCont.classList.add('mobile-menu-container--active');
+        document.body.style.overflow = 'hidden';
+    }
+    if (activeClass) {
+        mobileMenuCont.classList.remove('mobile-menu-container--active');
+        mobileMenuCont.classList.remove('mobile-menu-container--active');
+        document.body.style.overflow = 'auto';
+    }
 });
+
+for (let i = 0; i < mobileLink.length; i++) {
+    mobileLink[i].addEventListener('click', function () {
+        mobileMenuCont.classList.remove('mobile-menu-container--active');
+        document.body.style.overflow = 'auto';
+    });
+}
+
 
 // TEAM ACCO
 
 const accoTeam = document.getElementsByClassName('accordeon-team__desc-people');
 
 for (let i = 0; i < accoTeam.length; i++) {
-  accoTeam[i].addEventListener('click', function () {
-    let activeClass = this.classList.contains('accordeon-team__desc-people--active');
-    if (!activeClass) {
-      this.classList.add('accordeon-team__desc-people--active');
-    }
-    if (activeClass) {
-      this.classList.remove('accordeon-team__desc-people--active');
-    }
-  });
+    accoTeam[i].addEventListener('click', function () {
+        let activeClass = this.classList.contains('accordeon-team__desc-people--active');
+        if (!activeClass) {
+            this.classList.add('accordeon-team__desc-people--active');
+        }
+        if (activeClass) {
+            this.classList.remove('accordeon-team__desc-people--active');
+        }
+    });
 }
 
 // MENU ACCO
@@ -37,17 +49,17 @@ const accoMenuBtn = document.getElementsByClassName('accordeon-menu__list-btn');
 const accoMenuDesc = document.getElementsByClassName('accordeon-menu__hidden-desc');
 
 for (let i = 0; i < accoMenuBtn.length; i++) {
-  accoMenuBtn[i].addEventListener('click', function () {
-    for (let y = 0; y < accoMenuDesc.length; y++) {
-      let activeClass = accoMenuDesc[i].classList.contains('accordeon-menu__hidden-desc--active');
-      if (!activeClass) {
-        accoMenuDesc[i].classList.add('accordeon-menu__hidden-desc--active');
-      }
-      if (activeClass) {
-        accoMenuDesc[i].classList.remove('accordeon-menu__hidden-desc--active');
-      }
-    }
-  });
+    accoMenuBtn[i].addEventListener('click', function () {
+        for (let y = 0; y < accoMenuDesc.length; y++) {
+            let activeClass = accoMenuDesc[i].classList.contains('accordeon-menu__hidden-desc--active');
+            if (!activeClass) {
+                accoMenuDesc[i].classList.add('accordeon-menu__hidden-desc--active');
+            }
+            if (activeClass) {
+                accoMenuDesc[i].classList.remove('accordeon-menu__hidden-desc--active');
+            }
+        }
+    });
 }
 
 // BURGER MENU
@@ -59,11 +71,11 @@ let computedStyle = getComputedStyle(burgerMenuList);
 let maxOp = 1;
 
 burgerMenu.addEventListener('click', function () {
-  if (computedStyle.opacity < maxOp) {
-    burgerMenuList.style.opacity += 1;
-  } else {
-    burgerMenuList.style.opacity -= 1;
-  }
+    if (computedStyle.opacity < maxOp) {
+        burgerMenuList.style.opacity += 1;
+    } else {
+        burgerMenuList.style.opacity -= 1;
+    }
 
 });
 
@@ -80,94 +92,89 @@ let currentRight = 0;
 
 sliderRight.addEventListener('click', function (e) {
 
-  if (currentRight < maxRight) {
-    currentRight += step;
-    slider.style.right = currentRight + 'px';
-  }
+    if (currentRight < maxRight) {
+        currentRight += step;
+        slider.style.right = currentRight + 'px';
+    }
 });
 
 sliderLeft.addEventListener('click', function (e) {
 
-  if (currentRight > minRight) {
-    currentRight -= step;
-    slider.style.right = currentRight + 'px';
-  }
+    if (currentRight > minRight) {
+        currentRight -= step;
+        slider.style.right = currentRight + 'px';
+    }
 });
 
 // MAPS
 
 ymaps.ready(init);
 
-let placemarks = [
-  {
-    latitude: 59.97,
-    longitude: 30.31,
-    hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
-    balloonContent: [
-      '<div class="map__balloon">',
-      '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
-      'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 19',
-      '</div>'
-    ]
-  },
-  {
-    latitude: 59.94,
-    longitude: 30.25,
-    hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>',
-    balloonContent: [
-      '<div class="map__balloon">',
-      '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
-      'Самые вкусные бургеры у нас! Заходите по адресу: Малый проспект В О, д 64',
-      '</div>'
-    ]
-  },
-  {
-    latitude: 59.93,
-    longitude: 30.34,
-    hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
-    balloonContent: [
-      '<div class="map__balloon">',
-      '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
-      'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
-      '</div>'
-    ]
-  }
-],
-  geoObjects = [];
+let placemarks = [{
+        latitude: 59.97,
+        longitude: 30.31,
+        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 19',
+            '</div>'
+        ]
+    },
+        {
+            latitude: 59.94,
+            longitude: 30.25,
+            hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>',
+            balloonContent: [
+                '<div class="map__balloon">',
+                '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
+                'Самые вкусные бургеры у нас! Заходите по адресу: Малый проспект В О, д 64',
+                '</div>'
+            ]
+        },
+        {
+            latitude: 59.93,
+            longitude: 30.34,
+            hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+            balloonContent: [
+                '<div class="map__balloon">',
+                '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
+                'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+                '</div>'
+            ]
+        }
+    ],
+    geoObjects = [];
 
 function init() {
-  const map = new ymaps.Map('map', {
-    center: [59.94, 30.32],
-    zoom: 12,
-    controls: ['zoomControl'],
-    behaviors: ['drag']
-  });
+    const map = new ymaps.Map('map', {
+        center: [59.94, 30.32],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
 
-  for (let i = 0; i < placemarks.length; i++) {
-    geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
-      {
-        hintContent: placemarks[i].hintContent,
-        balloonContent: placemarks[i].balloonContent.join('')
-      },
-      {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/forsprite/svg/map-marker.svg',
-        iconImageSize: [46, 57],
-        iconImageOffset: [-23, -57]
-      });
-  }
+    for (let i = 0; i < placemarks.length; i++) {
+        geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
+            hintContent: placemarks[i].hintContent,
+            balloonContent: placemarks[i].balloonContent.join('')
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: '../img/forsprite/svg/map-marker.svg',
+            iconImageSize: [46, 57],
+            iconImageOffset: [-23, -57]
+        });
+    }
 
-  const clusterer = new ymaps.Clusterer({
-    clusterIcons: [
-      {
-        href: '../img/forsprite/png/burger-promo.png',
-        size: [100, 100],
-        offset: [-50, -50]
-      }
-    ],
-    clusterIconContentLayout: null
-  });
+    const clusterer = new ymaps.Clusterer({
+        clusterIcons: [{
+            href: '../img/forsprite/png/burger-promo.png',
+            size: [100, 100],
+            offset: [-50, -50]
+        }],
+        clusterIconContentLayout: null
+    });
 
-  map.geoObjects.add(clusterer);
-  clusterer.add(geoObjects);
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
 }
