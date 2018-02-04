3631638a5226500feef2ed8f5891b1e77ce723fc
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-02-04 18:06:04 +0300

    message

--- js/common.js
+++ js/common.js
@@ -39,12 +39,12 @@ const accoMenuDesc = document.getElementsByClassName('accordeon-menu__hidden-des
 for (let i = 0; i < accoMenuBtn.length; i++) {
   accoMenuBtn[i].addEventListener('click', function () {
     for (let y = 0; y < accoMenuDesc.length; y++) {
-      let activeClass = accoMenuDesc[y].classList.contains('accordeon-menu__hidden-desc--active')
+      let activeClass = accoMenuDesc[i].classList.contains('accordeon-menu__hidden-desc--active');
       if (!activeClass) {
-        accoMenuDesc[y].classList.add('accordeon-menu__hidden-desc--active');
+        accoMenuDesc[i].classList.add('accordeon-menu__hidden-desc--active');
       }
       if (activeClass) {
-        accoMenuDesc[y].classList.remove('accordeon-menu__hidden-desc--active');
+        accoMenuDesc[i].classList.remove('accordeon-menu__hidden-desc--active');
       }
     }
   });
@@ -61,8 +61,7 @@ let maxOp = 1;
 burgerMenu.addEventListener('click', function () {
   if (computedStyle.opacity < maxOp) {
     burgerMenuList.style.opacity += 1;
-  }
-  else {
+  } else {
     burgerMenuList.style.opacity -= 1;
   }
 
@@ -94,3 +93,81 @@ sliderLeft.addEventListener('click', function (e) {
     slider.style.right = currentRight + 'px';
   }
 });
+
+// MAPS
+
+ymaps.ready(init);
+
+let placemarks = [
+  {
+    latitude: 59.97,
+    longitude: 30.31,
+    hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
+      'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 19',
+      '</div>'
+    ]
+  },
+  {
+    latitude: 59.94,
+    longitude: 30.25,
+    hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
+      'Самые вкусные бургеры у нас! Заходите по адресу: Малый проспект В О, д 64',
+      '</div>'
+    ]
+  },
+  {
+    latitude: 59.93,
+    longitude: 30.34,
+    hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      '<img class="map__burger-img" src="../img/forsprite/png/burger-promo.png" alt="Бургер"/>',
+      'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+      '</div>'
+    ]
+  }
+],
+  geoObjects = [];
+
+function init() {
+  const map = new ymaps.Map('map', {
+    center: [59.94, 30.32],
+    zoom: 12,
+    controls: ['zoomControl'],
+    behaviors: ['drag']
+  });
+
+  for (let i = 0; i < placemarks.length; i++) {
+    geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+      {
+        hintContent: placemarks[i].hintContent,
+        balloonContent: placemarks[i].balloonContent.join('')
+      },
+      {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/forsprite/svg/map-marker.svg',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-23, -57]
+      });
+  }
+
+  const clusterer = new ymaps.Clusterer({
+    clusterIcons: [
+      {
+        href: '../img/forsprite/png/burger-promo.png',
+        size: [100, 100],
+        offset: [-50, -50]
+      }
+    ],
+    clusterIconContentLayout: null
+  });
+
+  map.geoObjects.add(clusterer);
+  clusterer.add(geoObjects);
+}
