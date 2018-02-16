4f70a8d2e4fc37bef5918d28a4acd391e5cbabef
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-02-16 14:26:34 +0300

    message

--- js/common.js
+++ js/common.js
@@ -2,8 +2,8 @@
 
 // BUTTON ANIMATION
 
-// const currentWidth = document.body.offsetWidth;
-// const tablet = 768;
+const currentWidth = document.body.offsetWidth;
+const tabletWidth = 768;
 // const btnDown = document.querySelector('.JS-btnDown');
 //
 // if (currentWidth > 768) {
@@ -23,7 +23,7 @@ $('[data-index]').on('click', function (e) {
 $(".main").onepage_scroll({
     sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
     easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",// "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    animationTime: 300,             // AnimationTime let you define how long each section takes to animate
+    animationTime: 500,             // AnimationTime let you define how long each section takes to animate
     pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
     updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
     beforeMove: function (index) {
@@ -32,11 +32,30 @@ $(".main").onepage_scroll({
     },   // This option accepts a callback function. The function will be called after the page moves.
     loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
     keyboard: true,                  // You can activate the keyboard controls
-    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+    responsiveFallback: 768,        // You can fallback to normal page scroll by defining the width of the browser in which
     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
     // the browser's width is less than 600, the fallback will kick in.
     direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
 });
+
+$('.btn').on('click', function() {
+    if(currentWidth <= tabletWidth) {
+        $('.btn').attr('href', '#contacts');
+    }
+    else {
+        $('.btn').removeAttr('href', '#contacts');
+    }
+});
+
+$('.btn-down__link').on('click', function() {
+    if(currentWidth <= tabletWidth) {
+        $('.btn-down__link').attr('href', '#features');
+    }
+    else {
+        $('.btn-down__link').removeAttr('href', '#features');
+    }
+});
+
 
 // MOBILE MENU
 
