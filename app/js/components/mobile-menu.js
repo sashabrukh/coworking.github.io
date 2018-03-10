466a13341ef6f451d95f75159ee00166582a23e7
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-03-10 17:55:00 +0300

    blog page + about page

--- app/js/components/mobile-menu.js
+++ app/js/components/mobile-menu.js
@@ -6,26 +6,29 @@
   const mobileMenuLink = document.getElementsByClassName('mobile-menu__link');
   const mobileMenuBtn = document.querySelector('#mobile-btn');
 
-  mobileMenuCheck.addEventListener('click', function () {
-    let activeClass = mobileMenuCont.classList.contains('mobile-menu__container--active');
-    if (!activeClass) {
-      mobileMenuCont.classList.add('mobile-menu__container--active');
-      document.body.style.overflow = 'hidden';
-      mobileMenuBtn.classList.add('mobile-menu__btn--active');
-    }
-    if (activeClass) {
-      mobileMenuCont.classList.remove('mobile-menu__container--active');
-      document.body.style.overflow = 'auto';
-      mobileMenuBtn.classList.remove('mobile-menu__btn--active');
-    }
-  });
-
-  for (let i = 0; i < mobileMenuLink.length; i++) {
-    mobileMenuLink[i].addEventListener('click', function () {
-      mobileMenuCont.classList.remove('mobile-menu__container--active');
-      document.body.style.overflow = 'auto';
-      mobileMenuBtn.classList.remove('mobile-menu__btn--active');
+  if (mobileMenuCheck) {
+    mobileMenuCheck.addEventListener('click', function () {
+      let activeClass = mobileMenuCont.classList.contains('mobile-menu__container--active');
+      if (!activeClass) {
+        mobileMenuCont.classList.add('mobile-menu__container--active');
+        document.body.style.overflow = 'hidden';
+        mobileMenuBtn.classList.add('mobile-menu__btn--active');
+      }
+      if (activeClass) {
+        mobileMenuCont.classList.remove('mobile-menu__container--active');
+        document.body.style.overflow = 'auto';
+        mobileMenuBtn.classList.remove('mobile-menu__btn--active');
+      }
     });
+
+    for (let i = 0; i < mobileMenuLink.length; i++) {
+      mobileMenuLink[i].addEventListener('click', function () {
+        mobileMenuCont.classList.remove('mobile-menu__container--active');
+        document.body.style.overflow = 'auto';
+        mobileMenuBtn.classList.remove('mobile-menu__btn--active');
+      });
+    }
   }
+
 
 })();
