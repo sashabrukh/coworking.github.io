438ab5b4f3b436e27c96c1f47e287ca4067af90b
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-03-28 17:13:03 +0300

    preloader

--- app/js/preloader/preloader.js
+++ app/js/preloader/preloader.js
@@ -3,14 +3,14 @@
 const preloader = (function () {
   const preloadCont = $('.preloader-container');
   const svg = $('.preloader-icon');
+  var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
   return {
     set: function () {
       svg.animate({
-        strokeDashoffset: '0',
-        strokeDasharray: '1000'
-      }, 3000);
-      console.log(svg);
+        strokeDashoffset: '0'
+      }, 1000);
       document.body.style.overflow = 'hidden';
+
       window.addEventListener('load', function () {
         document.body.style.overflow = 'auto';
         preloadCont.hide();
