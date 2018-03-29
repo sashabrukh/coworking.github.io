4ec5b888888a1421d088ee51fd0dfcf6d78b9743
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-03-29 12:54:36 +0300

    welcome form refactoring

--- app/js/welcome-form/welcome-form.js
+++ app/js/welcome-form/welcome-form.js
@@ -22,23 +22,17 @@ const welcome = $('.welcome-form__container');
           if(welcome.hasClass(animback)) {
             welcome.removeClass(animback);
           }
-          welcome.hasClass(anim) ? welcome.removeClass(anim) : welcome.addClass(anim);
+          welcome.addClass(anim);
           $.when( link.animate({ opacity: 0.0 }, 700) ).then(function(){
             link.hide();
           })
         });
 
         linkBack.click(function (e) {
-          if(welcome.hasClass('run-index-animation')) {
-            console.log('sss')
-          }
-          if(welcome.hasClass('run-index-animation-back')) {
-            console.log('sss')
-          }
           e.preventDefault();
-          welcome.hasClass(animback) ? welcome.removeClass(animback) : welcome.addClass(animback);
-          $.when( link.animate({ opacity: 1 }, 700) ).then(function(){
-            link.show();
+          welcome.addClass(animback);
+          $.when( link.show() ).then(function(){
+            link.animate({ opacity: 1 }, 700)
           })
         });
 
