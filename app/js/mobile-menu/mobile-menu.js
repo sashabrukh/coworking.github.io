b9fa26e24ca498ed96438fd77c1f6e0c2b5ce668
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-04-01 18:09:12 +0300

    mob menu animate with promise

--- app/js/mobile-menu/mobile-menu.js
+++ app/js/mobile-menu/mobile-menu.js
@@ -1,41 +1,67 @@
 ;'use strict';
 
-(function mobileMenu() {
+const mobileMenuCont = document.querySelector('#mobile-cont');
+
+const mobMenu = (function mobileMenu() {
   const mobileMenuCheck = document.querySelector('#mobile-checkbox');
-  const mobileMenuCont = document.querySelector('#mobile-cont');
-  const mobileMenuLink = document.getElementsByClassName('mobile-menu__link');
+  const mobileMenuLink = document.querySelectorAll('.mobile-menu__link');
   const mobileMenuBtn = document.querySelector('#mobile-btn');
+  const textOpacity = [{
+    opacity: 0.5
+  }];
 
-  if (mobileMenuCheck) {
-    mobileMenuCheck.addEventListener('click', function () {
-      let activeClass = mobileMenuCont.classList.contains('mobile-menu__container--active');
-      if (!activeClass) {
-        mobileMenuCont.classList.add('mobile-menu__container--active');
-        document.body.style.overflow = 'hidden';
-        mobileMenuBtn.classList.add('mobile-menu__btn--active');
+  return {
+    init: function () {
+      mobileMenuCheck.addEventListener('click', function () {
+        const activeClass = mobileMenuCont.classList.contains('mobile-menu__container--active');
+        if (!activeClass) {
+          mobileMenuCont.classList.add('mobile-menu__container--active');
+          document.body.style.overflow = 'hidden';
+          mobileMenuBtn.classList.add('mobile-menu__btn--active');
+        }
+        if (activeClass) {
+          mobileMenuCont.classList.remove('mobile-menu__container--active');
+          document.body.style.overflow = 'auto';
+          mobileMenuBtn.classList.remove('mobile-menu__btn--active');
+        }
+      });
+
+
+    },
+
+    animate: function () {
+      function animateText(index) {
+        const promise = new Promise(function (resolve) {
+          mobileMenuLink[index].style.opacity = 1;
+          setTimeout(function () {
+            resolve();
+          }, 500);
+        });
+        return promise;
       }
-      if (activeClass) {
-        mobileMenuCont.classList.remove('mobile-menu__container--active');
-        document.body.style.overflow = 'auto';
-        mobileMenuBtn.classList.remove('mobile-menu__btn--active');
-      }
-    });
 
-    if (mobileMenuCont) {
-      mobileMenuCont.addEventListener('click', function (e) {
-        mobileMenuCont.classList.remove('mobile-menu__container--active');
-        document.body.style.overflow = 'auto';
-        mobileMenuBtn.classList.remove('mobile-menu__btn--active');
-      })
-    }
-
-    for (let i = 0; i < mobileMenuLink.length; i++) {
-      mobileMenuLink[i].addEventListener('click', function () {
-        mobileMenuCont.classList.remove('mobile-menu__container--active');
-        document.body.style.overflow = 'auto';
-        mobileMenuBtn.classList.remove('mobile-menu__btn--active');
-      })
+      mobileMenuCheck.addEventListener('click', function () {
+        const timerId = setTimeout(function () {
+          animateText(0)
+            .then(function () {
+              return animateText(1)
+            }).then(function () {
+            return animateText(2)
+          }).then(function () {
+            return animateText(3)
+          })
+        }, 500);
+      });
     }
   }
 
 })();
+
+if (mobileMenuCont) {
+  mobMenu.init();
+  mobMenu.animate();
+}
+
+
+
+
