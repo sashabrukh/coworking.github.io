fe9628eff9143145f07bbd755e1f358d8c3f35cf
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-04-10 22:19:52 +0300

    refactoring

--- app/js/preloader/preloader.js
+++ app/js/preloader/preloader.js
@@ -1,66 +1,66 @@
 ;'use strict';
-
-const preloader = (function () {
+  
+const preloader = (function() {
+  const preloadImg = document.getElementsByTagName('*');
+  const preloadBlock = document.querySelector('.preloader-container');
+  const preloadText = document.querySelector('.preloader-text');
+  const regImgUrl = /background-image: url\(['"]?(.*?)['"]?\)/i;
   const body = document.body;
-  const preloadCont = document.querySelector('.preloader-container');
-  const preloaderText = document.querySelector('.preloader-text');
-  let img = document.querySelectorAll('img');
-  let progress = 0;
-  let loadedImg = 0;
   const imgArr = [];
-
-
-  for (let i = 0; i < img.length; i++) {
-      imgArr.push(img[i]);
-  }
-
-  for (var k = 0; k < imgArr.length; k++) {
-    
-  }
-
-  console.log('count of images: ' + imgArr.length);
-
-
-  const allImg = 100 / imgArr.length;
-
+  let progress = 0;
+  let img;
+  let allImg;
+  let shadowImg;
 
   return {
-    set: function () {
-      return new Promise(function (resolve, reject) {
-        body.style.overflow = 'hidden';
-        preloadCont.style.display = 'block';
-        console.log(imgArr);
-        for (let j = 0; j < imgArr.length; j++) {
-          imgArr[j].onload = function () {
+    shadowImgAdd: function() {
+      shadowImg = document.createElement('img');
+      shadowImg.setAttribute('src', img);
+      imgArr.push(shadowImg);
 
-            progress += allImg;
-            loadedImg += 1;
-            console.log(Math.ceil(progress))
-            console.log( imgArr[j] + ' ' + loadedImg +  ' image is load');
-            if (progress < 100) {
-              preloaderText.innerText = Math.ceil(progress) + '%';
-            } else {
-              progress = 100;
-              preloaderText.innerText = Math.ceil(progress) + '%';
-            }
-            if (loadedImg === imgArr.length || Math.ceil(progress) === 100) {
-              resolve();
-            }
+    },
+
+    show: function() {
+      preloadBlock.style.display = 'block';
+      body.style.overflow = 'hidden';
+
+      return new Promise((resolve, reject) => {
+        for (let i = 0; i < preloadImg.length; i++) {
+          if (preloadImg[i].matches('img')) {
+            img = preloadImg[i].getAttribute('src');
+            this.shadowImgAdd();
+          } else if (preloadImg[i].hasAttribute('style') && regImgUrl.test(preloadImg[i].getAttribute('style'))) {
+            img = (preloadImg[i].getAttribute('style').slice(22, -2));
+            this.shadowImgAdd();
+          }
+
+          allImg = 100 / imgArr.length;
+
+          for (let j = 0; j < imgArr.length; j++) {
+            // preloadText.innerText = `${Math.ceil(progress)}%`;
+            imgArr[j].onload = function () {
+              progress += allImg;
+              if (progress < 100) {
+                preloadText.innerText = Math.ceil(progress) + '%';
+              } else {
+                progress = 100;
+                preloadText.innerText = progress + '%';
+              }
+
+              if(Math.ceil(progress) === 100) {
+                resolve();
+              }
+            };
           }
         }
-      }).then(function () {
-        preloadCont.style.display = 'none';
+      }).then(function() {
+        preloadBlock.style.display = 'none';
         body.style.overflow = 'auto';
-      })
-    }
-  }
+      });
+    },
+  };
 })();
 
-preloader.set();
-
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", function() {
+preloader.show();
+});
