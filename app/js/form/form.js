65b8bd3ade6e228cf4b37c1a6d0111fc26ba0a3d
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-04-18 17:49:17 +0300

    refactoring

--- app/js/form/form.js
+++ app/js/form/form.js
@@ -1,39 +1,23 @@
 ;'use strict';
 
-$('#submit-form').on('submit', submitForm);
-
-function submitForm(ev) {
-  ev.preventDefault();
-
-  var form = $(ev.target),
-    data = form.serialize(),
-    url = form.attr('action'),
-    type = form.attr('method');
-
-  ajaxForm(form).done(function (msg) {
-    var mes = msg.mes,
-      status = msg.status;
-
-    if (status === 'OK') {
-      form.append('<p class="success">' + mes + '</p>');
-    } else {
-      form.append('<p class="error">' + mes + '</p>');
-    }
-  }).fail(function (jqXHR, textStatus) {
-    alert("Request failed: " + textStatus);
+$(document).ready(function () {
+  var popup = $('.my-works__contacts-form-popup');
+  popup.hide();
+  //E-mail Ajax Send
+  $("#submit-form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "../../server.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      popup.show();
+      setTimeout(function () {
+        popup.hide();
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
   });
 
-}
-
-// Универсальная функция для работы с формами
-var ajaxForm = function (form) {
-  var data = form.serialize(),
-    url = form.attr('action');
-
-  return $.ajax({
-    type: 'POST',
-    url: url,
-    dataType: 'JSON',
-    data: data
-  })
-};
+});
